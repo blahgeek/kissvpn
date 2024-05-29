9a7cdde278dbe5d3f8b307fa90939c6177a270fc
Yikai Zhao <yikai@z1k.dev>
2024-05-29 23:59:07 +0800

    tun: get final name

--- src/tun.rs
+++ src/tun.rs
@@ -4,12 +4,13 @@ use anyhow::Result;
 
 pub struct TunDevice {
     fd: File,
+    name: String,
 }
 
 
 #[repr(C)]
 struct Ifreq {
-    pub ifrn_name: [std::ffi::c_uchar; 16],
+    pub ifrn_name: [std::ffi::c_char; 16],
     pub ifru_flags: std::ffi::c_short,
 }
 
@@ -23,22 +24,30 @@ impl TunDevice {
             .open("/dev/net/tun")?;
 
         let mut ifreq = Ifreq {
-            ifrn_name: std::default::Default::default(),
+            ifrn_name: [0; 16],
             ifru_flags: 0x1001,  // IFF_TUN | IFF_NO_PI
         };
 
         let ifname_bytes = ifname.as_ref().as_bytes();
-        unsafe {
-            nix::libc::memcpy(ifreq.ifrn_name.as_mut_ptr() as *mut std::ffi::c_void,
-                              ifname_bytes.as_ptr() as *const std::ffi::c_void,
-                              usize::min(16, ifname_bytes.len()));
-            ifreq.ifrn_name[15] = 0;
-            tun_set_iff(fd.as_raw_fd(), &ifreq as *const Ifreq as u64)?;
-        }
+        let result_name =
+            unsafe {
+                nix::libc::memcpy(ifreq.ifrn_name.as_mut_ptr() as *mut std::ffi::c_void,
+                                  ifname_bytes.as_ptr() as *const std::ffi::c_void,
+                                  usize::min(15, ifname_bytes.len()));
+                tun_set_iff(fd.as_raw_fd(), &ifreq as *const Ifreq as u64)?;
+
+                std::ffi::CStr::from_ptr(ifreq.ifrn_name.as_ptr())
+                    .to_string_lossy().into_owned()
+            };
 
         Ok(TunDevice {
             fd,
+            name: result_name,
         })
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
     }
 }
 
