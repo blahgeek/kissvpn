2d4dc84b6da80b520789b8ff5b5257bf304e94f8
Yikai Zhao <yikai@z1k.dev>
2024-05-30 23:28:42 +0800

    add random padding as obfs

--- src/cipher.rs
+++ src/cipher.rs
@@ -1,8 +1,13 @@
+use core::slice;
+
 use aead::Buffer;
 use anyhow::Result;
+use rand::RngCore;
 use sha2::Sha256;
 use hkdf::Hkdf;
 use chacha20poly1305::{ChaCha8Poly1305, KeyInit, AeadCore, AeadInPlace, Nonce};
+
+use crate::constants::TRANSPORT_MTU;
 
 #[derive(Clone)]
 pub struct Cipher {
@@ -32,13 +37,33 @@ impl Cipher {
         self.chacha20.encrypt_in_place(&nonce, &[], buf)?;
         buf.extend_from_slice(&nonce)?;
 
+        // obfs. pad random 1 to 255 bytes to the end.
+        // the last byte represents count of bytes added
+        let n_random_bytes = i32::min(255, TRANSPORT_MTU as i32 - buf.len() as i32 - 1);
+        assert!(n_random_bytes >= 0);
+
+        let mut random_bytes = [0u8; 255];
+        if n_random_bytes > 0 {
+            rng.fill_bytes(&mut random_bytes[0..n_random_bytes as usize]);
+            buf.extend_from_slice(&random_bytes[0..n_random_bytes as usize])?;
+        }
+        let n_random_bytes_u8 = n_random_bytes as u8;
+        buf.extend_from_slice(slice::from_ref(&n_random_bytes_u8))?;
+
         Ok(())
     }
 
     pub fn decrypt(&self, buf: &mut impl Buffer) -> Result<()> {
-        if buf.len() < NONCE_SIZE {
+        if buf.len() < 1 {
             anyhow::bail!("Invalid length {}", buf.len());
         }
+        let n_random_bytes = buf.as_ref()[buf.len() - 1] as usize;
+
+        if buf.len() < NONCE_SIZE + 1 + n_random_bytes {
+            anyhow::bail!("Invalid length {}, n random bytes = {}", buf.len(), n_random_bytes);
+        }
+        buf.truncate(buf.len() - 1 - n_random_bytes);
+
         let nonce = Nonce::from_slice(&buf.as_ref()[(buf.len() - NONCE_SIZE) ..]).clone();
         buf.truncate(buf.len() - NONCE_SIZE);
 
@@ -63,7 +88,8 @@ mod tests {
             buf.reserve(100);
             cipher.encrypt(&mut buf)?;
 
-            assert_eq!(buf.len(), 12 + NONCE_SIZE + 16);
+            assert!(buf.len() > 12 + NONCE_SIZE + 16);
+            assert!(buf.len() <= 12 + NONCE_SIZE + 16 + 256);
             buf
         };
 
--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,6 @@
 pub const VPN_MTU: usize = 1300;
-pub const TRANSPORT_MTU: usize = VPN_MTU + 28;  // 12 nonce, 16 mac
+
+// the encryption requires extra 28 bytes (12 nonce, 16 mac). remaining bytes are for obfs.
+pub const TRANSPORT_MTU: usize = 1400;
+
 pub const UDP_MTU: usize = 1480;
