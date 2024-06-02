51c64cf56b7cee7238427557829fa23dce8d1205
Yikai Zhao <yikai@z1k.dev>
2024-06-02 21:24:30 +0800

    fix mtu

--- src/constants.rs
+++ src/constants.rs
@@ -4,14 +4,14 @@ pub const VPN_MTU: usize = 1362;
 // the encryption requires extra 28 bytes (12 nonce, 16 mac).
 // remaining bytes are for obfs.
 
-pub const TRANSPORT_MTU: usize = 1400;
+pub const TRANSPORT_MTU: usize = 1392;
 
 static_assertions::const_assert!(VPN_MTU + 12 + 16 <= TRANSPORT_MTU);
 
-// PPPoE MTU = 1492, IPv4 header = 20
+// PPPoE MTU = 1492, IPv4 header = 20, UDP header = 8
 // TODO: no support for ipv6 for now
 // So use UDP MTU of 1472
-pub const UDP_MTU: usize = 1472;
+pub const UDP_MTU: usize = 1464;
 
 
 pub const BUF_CAPACITY: usize = 1500;
